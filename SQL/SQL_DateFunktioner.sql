e77045245cc9904833724b2ba28a431908c1480e
albinostholm <albin.ostholm@gmail.com>
2016-11-29 08:38:46 +0100

    Add files via upload
    Lagt till alla Querys som är relevanta

---
+++ SQL/SQL_DateFunktioner.sql
@@ -0,0 +1,32 @@
+llINSERT INTO anstalld_schema
+(anstalldID,startTid,slutTid,datum,CreatedBy)
+SELECT anstalldID,startTid,slutTid,datum,CreatedBy from uppdaterat_schema$
+
+go
+
+select * from anstalld_schema
+
+
+SELECT 
+	startTid,
+	DATEPART(ww, startTid) AS vecka,
+	DATEPART(dd, startTid) AS datum,
+	CONVERT(VARCHAR(5),GETDATE(),114) 'hh:mi:ss:mmm'
+	--CONVERT(nvarchar(5), DATEPART(hh, startTid)) + ':' + CONVERT(nvarchar(5), DATEPART(MI, startTid)) AS Starttid
+FROM anstalld_schema
+where schemaID=1
+
+
+
+SELECT Convert(Datetime, GetDate(), 108)
+
+
+
+SELECT 
+	GETDATE() 'Today', 
+	CONVERT(VARCHAR(5),GETDATE(),114) 'hh:mi:ss:mmm'
+
+
+
+
+
