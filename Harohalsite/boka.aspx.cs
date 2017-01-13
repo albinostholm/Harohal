c68c68f3fe4eff535d130746030a8eacb98fc9e3
Trehorna123 <trehorna@gmail.com>
2017-01-13 14:17:16 +0100

    Boka hype

--- Harohalsite/boka.aspx.cs
+++ Harohalsite/boka.aspx.cs
@@ -139,7 +139,7 @@ public partial class boka : Page
     private void FillCalender()
     {
         DayPilotCalendar1.StartDate = DateTimeExtensions.FirstDateOfWeekISO8601(int.Parse(hfYear.Value), int.Parse(hfWeek.Value));
-        DayPilotCalendar1.DataSource = calenderEvents(DayPilotCalendar1.StartDate, int.Parse(hfWeek.Value) + 1);
+        DayPilotCalendar1.DataSource = calenderEvents(DayPilotCalendar1.StartDate, int.Parse(hfWeek.Value));
         DayPilotCalendar1.DataBind();
     }
 
