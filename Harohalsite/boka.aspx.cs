be81a81ca969c8e7a18d5dfc9ccf79c6292faec1
Trehorna123 <trehorna@gmail.com>
2017-02-22 10:55:52 +0100

    Fixed uppdating schedule on boka when changing massor

--- Harohalsite/boka.aspx.cs
+++ Harohalsite/boka.aspx.cs
@@ -247,8 +247,6 @@ public partial class boka : Page
     //Laddar om schemat ifall man byter massör
     protected void ddlMassor_SelectedIndexChanged(object sender, EventArgs e)
     {
-        DayPilotCalendar1.StartDate = DateTimeExtensions.FirstDateOfWeekISO8601(int.Parse(hfYear.Value), int.Parse(hfWeek.Value));
-        DayPilotCalendar1.DataSource = calenderEvents(DayPilotCalendar1.StartDate, int.Parse(hfWeek.Value) + 1);
-        DayPilotCalendar1.DataBind();
+        FillCalender();
     }
 }
